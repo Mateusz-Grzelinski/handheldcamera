5eae35f675524ba4cd8be78673dba8b35eaceedc
Mateusz Grzelinski <grzelinskimat@gmail.com>
2018-12-28 14:26:13 +0100

    Add docs

--- run_blender.sh
+++ run_blender.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-# connnect to debugger automatically
+# run blender and connnect to pycharm debugger automatically
+# see: https://code.blender.org/2015/10/debugging-python-code-with-pycharm/
 blender --python-expr "import bpy; bpy.ops.debug.connect_debugger_pycharm()"
